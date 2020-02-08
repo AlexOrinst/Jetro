c9b53641210b3e61e518b7e52f91fd1256fd6114
AlexOrinst <koluchiy8@gmail.com>
2020-02-08 12:13:52 +0300

    added project files

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 6,
+        slidesToScroll: 2,
+        asNavFor: '.slider-for',
+        centerMode: false,
+        focusOnSelect: true,
+        // variableWidth: true,
+        arrows: false,
+    });
+
+    $('.header__menu-btn').on('click', function(){
+        $('.header__menu ul').slideToggle();
+    });
+});
